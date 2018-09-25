b40f00e53c79f036299103f6dae059748ce9cdd7
Dalla Dirosa <dalladirosa@gmail.com>
2018-09-25 23:01:52 +0700

    Final redux authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { setCurrentUser } from './actions/authAction'
+import { setCurrentUser, logoutUser } from './actions/authAction'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
 import store from './store'
@@ -22,6 +22,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded))
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser)
+    // TODO: Clear current profile
+    // Redirect to login
+    window.location.href = '/login'
+  }
 }
 
 class App extends Component {
