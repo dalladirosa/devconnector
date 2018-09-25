b2d82f3b62047681415e59ce51b7f04b32c379b9
Dalla Dirosa <dalladirosa@gmail.com>
2018-09-25 15:48:45 +0700

    Add token in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { setCurrentUser } from './actions/authAction'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import store from './store'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+
 import './App.css'
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken)
+  // Decode token and get user info and explanation
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     )
   }
 }
